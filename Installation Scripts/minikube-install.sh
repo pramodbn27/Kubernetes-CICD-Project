527df37a33b7e972f6b774d1ff6dc7949af83dae
pramodbn <begurnagaraj.p@northeastern.edu>
2023-12-13 23:41:03 -0500

    Installation Scripts

---
+++ Installation Scripts/minikube-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo su
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt-get install docker-ce -y
+docker version
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl wget apt-transport-https
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube version
